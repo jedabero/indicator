9d67ba6f9e415b3f2c8e38dbb522127b71b3d665
jedabero <jedabero20@gmail.com>
2017-06-19 07:54:02 -0500

    Cuarto commit
    Carpeta con scripts

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # indicator
 
 Ubuntu App Indicator in python
+
+## `scripts` folder
+
+All scripts in this folder must have correct shebangs
+
+```sh
+#!/usr/bin/env bash
+
+ping 8.8.8.8 -c 5
+```
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import signal
 import gi
+from os import listdir
+from os.path import isfile, join, dirname, splitext
 from subprocess import check_output
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 from gi.repository import AppIndicator3
 from gi.repository import Notify
 
 APP_ID = 'com.jedabero.indicator'
 
 
-def app_test(_):
-    out = check_output(['./test.sh'])
-    label = Gtk.Label(out.decode())
-    win = Gtk.Window(title="Resultado")
-    win.add(label)
+def exec(_, script):
+    # Buscar la carpeta de scripts
+    path = join(dirname(__file__), 'scripts')
+    # Ejecutar el script
+    out = check_output([path + '/' + script])
+    show_script_result(script, out.decode())
+
+
+def show_script_result(script_name, output):
+    label = Gtk.Label(output)
+    win = Gtk.Window(title="Resultado " + script_name)
+    scroll = Gtk.ScrolledWindow()
+    scroll.add(label)
+    win.add(scroll)
     win.show_all()
-    """Notify.Notification.new('Output', out.decode(), None).show()"""
+    # Obtener tamaño posible del resultado
+    size = label.size_request()
+    # Obtener tamaño de la pantalla actual
+    screen = Gdk.Screen.get_default()
+    monitor = screen.get_monitor_at_window(screen.get_active_window())
+    geometry = screen.get_monitor_geometry(monitor)
+    if size.width < geometry.width or size.height < geometry.height:
+        # Redimensionar si el tamaño posible es menor que el espacio disponible
+        win.resize(size.width, size.height)
+    else:
+        # Maximizar cuando el output sea muy grande
+        win.maximize()
+    Notify.Notification.new('Done ' + script_name, "Script " + script_name + " ended execution", None).show()
 
 
 def app_quit(_):
@@ -26,23 +49,27 @@ def app_quit(_):
     Gtk.main_quit()
 
 
-def make_menu(text, callback):
+def make_menu(text, callback, arg=None):
     item_test = Gtk.MenuItem(text)
-    item_test.connect('activate', callback)
+    if arg:
+        item_test.connect('activate', callback, arg)
+    else:
+        item_test.connect('activate', callback)
     return item_test
 
 
 def build_menu():
     menu = Gtk.Menu()
-    menu.append(make_menu('Ejecutar script', app_test))
-    menu.append(make_menu('Salir', app_quit))
-    """
-    submenuitem = Gtk.MenuItem('Submenu')
+    path = join(dirname(__file__), 'scripts')
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    submenuitem = Gtk.MenuItem('Scripts')
     submenu = Gtk.Menu()
-    submenu.append(make_menu('Ejecutar script', app_test))
-    submenuitem.set_submenu(submenu)
+    for script in onlyfiles:
+        script_name, _ = splitext(script)
+        submenu.append(make_menu(script_name.replace("_", " ").capitalize(), exec, script))
+        submenuitem.set_submenu(submenu)
     menu.append(submenuitem)
-    """
+    menu.append(make_menu('Salir', app_quit))
     menu.show_all()
     return menu
 
